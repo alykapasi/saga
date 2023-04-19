6ae391b7a94a0f45a787ba780551f47dfabdfe49
Aly Kapasi <122089907+alykapasi@users.noreply.github.com>
2023-04-19 04:02:55 -0700

    removed the web page front end -> focus on py app

--- test.py
+++ test.py
@@ -3,16 +3,17 @@ import openai
 
 openai.api_key = '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'
 
-# Combine conversation history into a single string
-messages = [{'role': 'user', 'content': 'Write a story about Humpty-Dumpty and the backstory to his fall in murder mystery format'}]
+# First determine whether the user wants to modify a story
+# or create on from scratch
+starting_message = [{'role': 'user', 'content': 'Hello!'}]
 
 completion = openai.ChatCompletion.create(
     model='gpt-3.5-turbo',
-    messages=messages,
-    temperature=1.0,
-    max_tokens=1000,
-    presence_penalty=0.75,
-    frequency_penalty=0.75,
+    messages=starting_message,
+    temperature=0,
+    max_tokens=100,
+    presence_penalty=0,
+    frequency_penalty=0,
 )
 
 print(completion.choices[0].message['content'])
